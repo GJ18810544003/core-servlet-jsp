a156e392365bc6354da20f659d81534cd6d21629
郭俊 <guojun@douyu.tv>
2019-04-01 20:18:45 +0800

    Initial commit

---
+++ src/test/java/com/servlet/core/servlet/jsp/CoreServletJspApplicationTests.java
@@ -0,0 +1,16 @@
+package com.servlet.core.servlet.jsp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CoreServletJspApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
